657c0181f8569abd710f103d41e91fe98d1addfd
Raghvendra Singh Shekhawat <163868970+raghvendra-singh9@users.noreply.github.com>
2024-11-07 01:21:06 +0530

    Add files via upload

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -173,7 +173,7 @@ const Layout = ({
           href="/images/favicon.png"
           type="image/x-icon"
         />
-        <title>Xpovio | Digital Agency Creative Portfolio Template</title>
+        <title>Daiichi Digital | Fueling Growth</title>
         <meta name="keywords" content="creative, agency, portfolio" />
         <meta
           name="description"
