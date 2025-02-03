bc5b52ab897959d53176241217fa4e7d4ad6030e
Raghvendra Singh Shekhawat <163868970+raghvendra-singh9@users.noreply.github.com>
2025-02-03 23:08:03 +0530

    Add files via upload

--- src/pages/service-single.tsx
+++ src/pages/service-single.tsx
@@ -1,15 +1,39 @@
-import React from "react";
-import Layout from "@/components/layout/Layout";
-import ServiceDetailsMain from "@/components/containers/service-details/ServiceDetailsMain";
-import ServiceDetailsBanner from "@/components/layout/banner/ServiceDetailsBanner";
+import React from 'react';
+import Layout from '@/components/layout/Layout';
+import ServiceDetailsMain from '@/components/containers/service-details/ServiceDetailsMain';
+import ServiceDetailsBanner from '@/components/layout/banner/ServiceDetailsBanner';
 // import UxProcess from "@/components/containers/service-details/UxProcess";
-import CtaTwo from "@/components/containers/service-details/CtaTwo";
+import CtaTwo from '@/components/containers/service-details/CtaTwo';
+import ServiceData from '@/components/containers/service-details/data/serviceData.json';
 
-const ServiceDetails = () => {
+// Define the TypeScript type for a single service
+interface Service {
+  id: string;
+  title: string;
+  description: string[];
+  approachTitle: string;
+  approachContent: string[];
+  images: {
+    thumbOne: string;
+    thumbTwo: string;
+  };
+}
+
+interface ServiceDetailsProps {
+  service: Service | null;
+}
+const ServiceDetails = ({ service }: ServiceDetailsProps) => {
+  if (!service) {
+    return <p>Service not found.</p>;
+  }
   return (
     <Layout header={2} footer={2} video={false}>
-      <ServiceDetailsBanner />
-      <ServiceDetailsMain />
+      {/* Pass the dynamic title to the banner */}
+      <ServiceDetailsBanner title={service.title} />
+
+      {/* Pass the service details */}
+      <ServiceDetailsMain data={service} />
+
       {/* <UxProcess /> */}
       <CtaTwo />
     </Layout>
